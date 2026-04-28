d9e30953dab61adc64de21d5ec55ffaf6429cb3f
ivanchristian126 <ivan.christian006@binus.ac.id>
2026-04-28 16:31:39 +0700

    feat:add navbar and footer layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,51 @@
+<footer class="bg-white w-full border-t border-gray-200 mt-20 py-10">
+    <div class="w-[95%] max-w-7xl mx-auto grid grid-cols-4 gap-8">
+        <div class="flex flex-col gap-5">
+            <a href="/">
+                <img src="{{ asset('images/logo/SUMORROW-LOGO-BLACK.png') }}" alt="Sumorrow Logo" class="h-12 w-auto">
+            </a>
+            <p class="text-gray-500 text-sm leading-relaxed max-w-[185px]">
+                Connecting the spirit of adventure with Indonesia's highest peaks and archipelago's majestic peaks
+            </p>
+        </div>
+
+        <div>
+            <h3 class="font-bold text-[#1a2b4c] mb-6">NAVIGATION</h3>
+            <div class="flex flex-col gap-4">
+                <a href="#" class="text-gray-500 hover:text-[#094174] transition w-fit">Home</a>
+                <a href="#" class="text-gray-500 hover:text-[#094174] transition w-fit">Explore</a>
+                <a href="#" class="text-gray-500 hover:text-[#094174] transition w-fit">Community</a>
+            </div>
+        </div>
+
+        <div>
+            <h3 class="font-bold text-[#1a2b4c] mb-6">LEGAL</h3>
+
+            <div class="flex flex-col gap-4">
+                <a href="#" class="text-gray-500 hover:text-[#094174] transition w-fit">Privacy Policy</a>
+                <a href="#" class="text-gray-500 hover:text-[#094174] transition w-fit">Terms of Service</a>
+            </div>
+        </div>
+
+        <div>
+            <h3 class="font-bold text-[#1a2b4c] mb-6">CONTACT US</h3>
+
+            <div class="flex flex-col gap-4">
+                <a href="mailto:himilsquad@gmail.com" class="flex items-center gap-3 group w-fit">
+                    <img src="{{ asset('images/socialmedia/email.png') }}" alt="Email Icon" class="w-5 h-5 object-contain">
+                    <span class="text-gray-500 group-hover:text-[#094174] transition">himilsquad@gmail.com</span>
+                </a>
+                <a href="#" class="flex items-center gap-3 group w-fit">
+                    <img src="{{ asset('images/socialmedia/instagram.png') }}" alt="Instagram Icon" class="w-5 h-5 object-contain">
+                    <span class="text-gray-500 group-hover:text-[#094174] transition">himilsquad.id</span>
+                </a>
+            </div>
+        </div>
+
+    </div>
+    <div class="border-t border-gray-300 pt-6 mt-8 flex justify-center pb-6">
+        <p class="text-gray-500 text-sm font-medium">
+            2026 &copy; himil.Ltd | All Rights Reserved
+        </p>
+    </div>
+</footer>
