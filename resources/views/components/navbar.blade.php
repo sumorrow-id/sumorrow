5ce5119f93ec69fe85db297c5a8175e1b7019854
ivanchristian126 <ivan.christian006@binus.ac.id>
2026-04-29 15:47:51 +0700

    feat:make a dummy explore page

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="absolute top-6 left-0 right-0 mx-auto w-[95%] rounded-2xl z-10 flex justify-between items-center bg-[#1A1A1A]/18 border border-white/20 px-6 py-4 text-white backdrop-blur-md shadow-lg">
-    <div class="flex text-xl font-bold gap-2">
-        <a href="#" class="px-4 py-2 rounded-lg transition hover:bg-[#094174]/20 hover:text-white text-white">Home</a>
-        <a href="#" class="px-4 py-2 rounded-lg transition hover:bg-[#094174]/20 hover:text-white text-white">Explore</a>
+    <div class="flex text-l font-bold gap-2">
+        <a href="/" class="px-4 py-2 rounded-lg transition hover:bg-[#094174]/20 hover:text-white text-white">Home</a>
+        <a href="/explore" class="px-4 py-2 rounded-lg transition hover:bg-[#094174]/20 hover:text-white text-white">Explore</a>
         <a href="#" class="px-4 py-2 rounded-lg transition hover:bg-[#094174]/20 hover:text-white text-white">Community</a>
     </div>
     <div class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ExploreController;
 
 Route::get('/', [HomeController::class,'index'])->name('home');
+Route::get('/explore',[ExploreController::class,'dummy'])->name('explore');
