1bb4207fed91754a6d044da4e67034ea0c4ca17d
chukynya <henry.christian7557@gmail.com>
2026-05-03 22:58:01 +0700

    refactor(routes): point explore route to index method instead of dummy

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ExploreController;
 
-Route::get('/', [HomeController::class,'index'])->name('home');
-Route::get('/explore',[ExploreController::class,'dummy'])->name('explore');
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/explore', [ExploreController::class, 'index'])->name('explore');
