0a1a30afe04d8f3bca8fc3ae0a7aec212e9bcc9d
leonardwgk <leonardwangka@gmail.com>
2026-05-04 21:18:50 +0700

    refactor: paginate explore page, search, dan side bar activation

--- app/Models/Mountain.php
+++ app/Models/Mountain.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Mountain extends Model
 {
@@ -49,5 +50,10 @@ class Mountain extends Model
     public function comments(): HasMany
     {
         return $this->hasMany(Comment::class);
+    }
+
+    public function province(): BelongsTo
+    {
+        return $this->belongsTo(Province::class);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,17 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <link
+        rel="icon"
+        type="image/png"
+        href="{{ asset('images/logo/SUMORROW-LOGO-M.png') }}"
+    />
     <title>Sumorrow - Summit Tommorow</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+    @vite (['resources/css/app.css','resources/js/app.js'])
 </head>
-    <body class="bg-[#E7E7E7] antialiased font-sans text-gray-900 flex flex-col min-h-screen">
-        <x-navbar />
-        <main class="flex-grow">
-            @yield('content')
-        </main>
-        <x-footer />
-    </body>
+<body
+    class="bg-[#E7E7E7] antialiased font-sans text-gray-900 flex flex-col min-h-screen"
+>
+    <x-navbar />
+    <main class="flex-grow">
+        @yield ('content')
+    </main>
+    <x-footer />
+</body>
 </html>
