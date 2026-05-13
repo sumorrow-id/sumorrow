fcbac4c1eaa0adad5fc01c5645bd31dc7109311d
chukynya <henry.christian7557@gmail.com>
2026-05-13 16:21:00 +0700

    feat(api): add bearer token

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,11 +10,12 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, HasUuids, Notifiable;
+    use HasApiTokens, HasFactory, HasUuids, Notifiable;
 
     public $incrementing = false;
     protected $keyType = 'string';
