a5972ea794537d3275a47c417fa5cd1c72872e58
Leonard Wangka <leonardwangka@gmail.com>
2026-05-26 10:20:03 +0700

    fix: fix error and conflict code

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -45,6 +45,7 @@ class ProfileController extends Controller
 
     public function update(Request $request)
     {
+        /** @var \App\Models\User $user */
         $user = Auth::user();
 
         $request->validate([
